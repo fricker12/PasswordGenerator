3cecd931858e46870bb1724137389f6a7a765e8a
EVHEN KOROLOV <fricker12@gmail.com>
2023-05-25 23:18:19 +0300

    fix password generator

--- password_generator.py
+++ password_generator.py
@@ -9,12 +9,76 @@ def generate_password(length, character_set):
     return ''.join(random.choice(character_set) for _ in range(length))
 
 
+#def generate_password_from_template_test(template, character_set):
+#    symbol_dict = {"d": string.digits,
+#                  "l": string.ascii_lowercase,
+#                  "L": string.ascii_uppercase + string.ascii_lowercase,
+#                  "u": string.ascii_uppercase,
+#                  "p": string.punctuation}
+#
+#    password = ''
+#    i = 0
+#    while i < len(template):
+#        if template[i] == '\\':
+#            if i + 1 < len(template):
+#                password += template[i + 1]
+#                i += 2
+#            else:
+#                password += template[i]
+#                i += 1
+#        elif template[i] == '{':
+#            closing_bracket_index = template.find('}', i)
+#            if closing_bracket_index != -1:
+#                placeholder = template[i + 1:closing_bracket_index]
+#                if placeholder.isnumeric():
+#                    repeat_count = int(placeholder)
+#                    password += password[-1] * (repeat_count - 1)
+#                else:
+#                    password += placeholder
+#                i = closing_bracket_index + 1
+#            else:
+#                password += template[i]
+#                i += 1
+#        elif template[i] == '[':
+#            closing_bracket_index = template.find(']', i)
+#            if closing_bracket_index != -1:
+#                custom_set = template[i + 1:closing_bracket_index]
+#                exclude_placeholders = []
+#                while '^' in custom_set:
+#                    caret_index = custom_set.index('^')
+#                    exclude_placeholders.append(custom_set[caret_index + 1])
+#                    custom_set = custom_set[:caret_index] + custom_set[caret_index + 2:]
+#                custom_set = [c for c in custom_set if c not in exclude_placeholders]
+#                if custom_set:
+#                    password += random.choice(custom_set)
+#                i = closing_bracket_index + 1
+#            else:
+#                password += template[i]
+#                i += 1
+#        else:
+#            # if template[i] == 'd':
+#            #     character_set = string.digits
+#            # elif template[i] == 'l':
+#            #     character_set = string.ascii_lowercase
+#            # elif template[i] == 'L':
+#            #     character_set = string.ascii_uppercase + string.ascii_lowercase
+#            # elif template[i] == 'u':
+#            #     character_set = string.ascii_uppercase
+#            # elif template[i] == 'p':
+#            #     character_set = string.#punctuation
+#
+#            character_set = symbol_dict.get(template[i])
+#            #password += ''.join(random.sample(character_set, len(character_set)))
+#            password +=random.choice(character_set) 
+#            i += 1
+#    return password
+
 def generate_password_from_template(template, character_set):
     symbol_dict = {"d": string.digits,
-                  "l": string.ascii_lowercase,
-                  "L": string.ascii_uppercase + string.ascii_lowercase,
-                  "u": string.ascii_uppercase,
-                  "p": string.punctuation}
+                   "l": string.ascii_lowercase,
+                   "L": string.ascii_uppercase + string.ascii_lowercase,
+                   "u": string.ascii_uppercase,
+                   "p": string.punctuation}
 
     password = ''
     i = 0
@@ -32,7 +96,18 @@ def generate_password_from_template(template, character_set):
                 placeholder = template[i + 1:closing_bracket_index]
                 if placeholder.isnumeric():
                     repeat_count = int(placeholder)
-                    password += password[-1] * (repeat_count - 1)
+                    if i > 0 and template[i - 1] == 'u':
+                        password += ''.join(random.sample(character_set, repeat_count-1))
+                    elif i > 0 and template[i - 1] == 'd':
+                        password += ''.join(random.sample(character_set, repeat_count-1))
+                    elif i > 0 and template[i - 1] == 'l':
+                        password += ''.join(random.sample(character_set, repeat_count-1))
+                    elif i > 0 and template[i - 1] == 'L':
+                        password += ''.join(random.sample(character_set, repeat_count-1))
+                    elif i > 0 and template[i - 1] == 'p':
+                        password += ''.join(random.sample(character_set, repeat_count-1))
+                    else:
+                        password += password[-1] * (repeat_count - 1)
                 else:
                     password += placeholder
                 i = closing_bracket_index + 1
@@ -56,19 +131,12 @@ def generate_password_from_template(template, character_set):
                 password += template[i]
                 i += 1
         else:
-            # if template[i] == 'd':
-            #     character_set = string.digits
-            # elif template[i] == 'l':
-            #     character_set = string.ascii_lowercase
-            # elif template[i] == 'L':
-            #     character_set = string.ascii_uppercase + string.ascii_lowercase
-            # elif template[i] == 'u':
-            #     character_set = string.ascii_uppercase
-            # elif template[i] == 'p':
-            #     character_set = string.punctuation
-
             character_set = symbol_dict.get(template[i])
-            password += random.choice(character_set)
+            password +=random.choice(character_set)
+            #if character_set:
+            #    last_char = password[-1] if password else ''
+            #    new_char = random.choice([c for c in character_set if c != last_char])
+            #    password += new_char
             i += 1
     return password
 
