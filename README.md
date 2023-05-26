a3e72ccf1a2dc586c106969f8e8814fae39088d1
EVHEN KOROLOV <fricker12@gmail.com>
2023-05-26 13:21:28 +0300

    add logger

--- README.md
+++ README.md
@@ -37,6 +37,25 @@ python password_generator.py -n 10 -p
 
 Verbose mode can be enabled using -v, -vv, or -vvv options to show detailed information during processing.
 
+example:
+python password_generator.py -t "u{4}d{3}\-l{2}" -vvv
+DEBUG:__main__:Verbose mode: Level 3
+INFO:__main__:Generating password from template: u{4}d{3}\-l{2}
+DEBUG:__main__:Generating password from template: u{4}d{3}\-l{2}
+DEBUG:__main__:Choosing character from set: ABCDEFGHIJKLMNOPQRSTUVWXYZ
+DEBUG:__main__:Generating uppercase characters: 3
+DEBUG:__main__:Choosing character from set: 0123456789
+DEBUG:__main__:Generating digit characters: 2
+DEBUG:__main__:Choosing character from set: abcdefghijklmnopqrstuvwxyz
+DEBUG:__main__:Generating lowercase characters: 1
+DEBUG:__main__:Generated password from template: DRKM681-fi
+INFO:__main__:Generated password: DRKM681-fi
+
+python password_generator.py -t "u{4}d{3}\-l{2}" -v
+INFO:__main__:Generating password from template: u{4}d{3}\-l{2}
+INFO:__main__:Generated password: KLSA102-vc
+
+
 The -H option displays the help information.
 
 
--- password_generator.py
+++ password_generator.py
@@ -5,80 +5,22 @@ import sys
 import logging
 
 
-def generate_password(length, character_set):
-    return ''.join(random.choice(character_set) for _ in range(length))
+def generate_password(length, character_set,logger):
+    logger.debug("Generating password of length %d", length)
+    password = ''.join(random.choice(character_set) for _ in range(length))
+    logger.debug("Generated password: %s", password)
+    return password
 
 
-#def generate_password_from_template_test(template, character_set):
-#    symbol_dict = {"d": string.digits,
-#                  "l": string.ascii_lowercase,
-#                  "L": string.ascii_uppercase + string.ascii_lowercase,
-#                  "u": string.ascii_uppercase,
-#                  "p": string.punctuation}
-#
-#    password = ''
-#    i = 0
-#    while i < len(template):
-#        if template[i] == '\\':
-#            if i + 1 < len(template):
-#                password += template[i + 1]
-#                i += 2
-#            else:
-#                password += template[i]
-#                i += 1
-#        elif template[i] == '{':
-#            closing_bracket_index = template.find('}', i)
-#            if closing_bracket_index != -1:
-#                placeholder = template[i + 1:closing_bracket_index]
-#                if placeholder.isnumeric():
-#                    repeat_count = int(placeholder)
-#                    password += password[-1] * (repeat_count - 1)
-#                else:
-#                    password += placeholder
-#                i = closing_bracket_index + 1
-#            else:
-#                password += template[i]
-#                i += 1
-#        elif template[i] == '[':
-#            closing_bracket_index = template.find(']', i)
-#            if closing_bracket_index != -1:
-#                custom_set = template[i + 1:closing_bracket_index]
-#                exclude_placeholders = []
-#                while '^' in custom_set:
-#                    caret_index = custom_set.index('^')
-#                    exclude_placeholders.append(custom_set[caret_index + 1])
-#                    custom_set = custom_set[:caret_index] + custom_set[caret_index + 2:]
-#                custom_set = [c for c in custom_set if c not in exclude_placeholders]
-#                if custom_set:
-#                    password += random.choice(custom_set)
-#                i = closing_bracket_index + 1
-#            else:
-#                password += template[i]
-#                i += 1
-#        else:
-#            # if template[i] == 'd':
-#            #     character_set = string.digits
-#            # elif template[i] == 'l':
-#            #     character_set = string.ascii_lowercase
-#            # elif template[i] == 'L':
-#            #     character_set = string.ascii_uppercase + string.ascii_lowercase
-#            # elif template[i] == 'u':
-#            #     character_set = string.ascii_uppercase
-#            # elif template[i] == 'p':
-#            #     character_set = string.#punctuation
-#
-#            character_set = symbol_dict.get(template[i])
-#            #password += ''.join(random.sample(character_set, len(character_set)))
-#            password +=random.choice(character_set) 
-#            i += 1
-#    return password
-
-def generate_password_from_template(template, character_set):
-    symbol_dict = {"d": string.digits,
-                   "l": string.ascii_lowercase,
-                   "L": string.ascii_uppercase + string.ascii_lowercase,
-                   "u": string.ascii_uppercase,
-                   "p": string.punctuation}
+def generate_password_from_template(template, character_set,logger):
+    logger.debug("Generating password from template: %s", template)
+    symbol_dict = {
+        "d": string.digits,
+        "l": string.ascii_lowercase,
+        "L": string.ascii_uppercase + string.ascii_lowercase,
+        "u": string.ascii_uppercase,
+        "p": string.punctuation
+    }
 
     password = ''
     i = 0
@@ -97,15 +39,20 @@ def generate_password_from_template(template, character_set):
                 if placeholder.isnumeric():
                     repeat_count = int(placeholder)
                     if i > 0 and template[i - 1] == 'u':
-                        password += ''.join(random.sample(character_set, repeat_count-1))
+                        logger.debug("Generating uppercase characters: %d", repeat_count - 1)
+                        password += ''.join(random.sample(character_set, repeat_count - 1))
                     elif i > 0 and template[i - 1] == 'd':
-                        password += ''.join(random.sample(character_set, repeat_count-1))
+                        logger.debug("Generating digit characters: %d", repeat_count - 1)
+                        password += ''.join(random.sample(character_set, repeat_count - 1))
                     elif i > 0 and template[i - 1] == 'l':
-                        password += ''.join(random.sample(character_set, repeat_count-1))
+                        logger.debug("Generating lowercase characters: %d", repeat_count - 1)
+                        password += ''.join(random.sample(character_set, repeat_count - 1))
                     elif i > 0 and template[i - 1] == 'L':
-                        password += ''.join(random.sample(character_set, repeat_count-1))
+                        logger.debug("Generating mixed case characters: %d", repeat_count - 1)
+                        password += ''.join(random.sample(character_set, repeat_count - 1))
                     elif i > 0 and template[i - 1] == 'p':
-                        password += ''.join(random.sample(character_set, repeat_count-1))
+                        logger.debug("Generating punctuation characters: %d", repeat_count - 1)
+                        password += ''.join(random.sample(character_set, repeat_count - 1))
                     else:
                         password += password[-1] * (repeat_count - 1)
                 else:
@@ -125,6 +72,7 @@ def generate_password_from_template(template, character_set):
                     custom_set = custom_set[:caret_index] + custom_set[caret_index + 2:]
                 custom_set = [c for c in custom_set if c not in exclude_placeholders]
                 if custom_set:
+                    logger.debug("Choosing character from custom set: %s", custom_set)
                     password += random.choice(custom_set)
                 i = closing_bracket_index + 1
             else:
@@ -132,16 +80,16 @@ def generate_password_from_template(template, character_set):
                 i += 1
         else:
             character_set = symbol_dict.get(template[i])
-            password +=random.choice(character_set)
-            #if character_set:
-            #    last_char = password[-1] if password else ''
-            #    new_char = random.choice([c for c in character_set if c != last_char])
-            #    password += new_char
+            logger.debug("Choosing character from set: %s", character_set)
+            password += random.choice(character_set)
             i += 1
+
+    logger.debug("Generated password from template: %s", password)
     return password
 
 
 def main():
+    
     parser = argparse.ArgumentParser(description='Password Generation Utility')
     parser.add_argument('-n', type=int,
                         help='Set length of password and generate random password from set {small lateral ASCII, big lateral ASCII, digit}')
@@ -163,10 +111,12 @@ def main():
     # Set logging level based on verbosity
     if verbose_level >= 3:
         logging.basicConfig(level=logging.DEBUG)
-    elif verbose_level >= 2:
+    elif verbose_level >= 1:
         logging.basicConfig(level=logging.INFO)
-    else:
+    elif verbose_level == 2:
         logging.basicConfig(level=logging.WARNING)
+    else:
+        logging.basicConfig(level=logging.INFO)
 
     logger = logging.getLogger(__name__)
 
@@ -178,30 +128,35 @@ def main():
         character_set = args.S
 
     if args.t:
-        if args.f:
-            print("Error: Both -t and -f options cannot be used together.")
-            sys.exit(1)
-        password = generate_password_from_template(args.t, character_set)
+        logger.info("Generating password from template: %s", args.t)
+        password = generate_password_from_template(args.t, character_set,logger)
         if args.p:
+            logger.debug("Permuting characters of the password")
             password = ''.join(random.sample(password, len(password)))
-        print(password)
+        logger.info("Generated password: %s", password)
     elif args.n:
         for _ in range(args.c):
-            password = generate_password(args.n, character_set)
+            logger.info("Generating password of length %d", args.n)
+            password = generate_password(args.n, character_set,logger)
             if args.p:
+                logger.debug("Permuting characters of the password")
                 password = ''.join(random.sample(password, len(password)))
-            print(password)
+            logger.info("Generated password: %s", password)
     elif args.f:
         try:
             with open(args.f, 'r') as file:
                 patterns = file.readlines()
             for pattern in patterns:
-                password = generate_password_from_template(pattern.strip(), character_set)
+                pattern = pattern.strip()
+                logger.info("Generating password from template: %s", pattern)
+                password = generate_password_from_template(pattern, character_set,logger)
                 if args.p:
+                    logger.debug("Permuting characters of the password")
                     password = ''.join(random.sample(password, len(password)))
-                print(password)
+                logger.info("Generated password: %s", password)
         except IOError:
             print("Error: Failed to open the file.")
+            logger.warning("Error: Failed to open the file.: %s", file)
             sys.exit(1)
     else:
         print("Error: No option specified.")
